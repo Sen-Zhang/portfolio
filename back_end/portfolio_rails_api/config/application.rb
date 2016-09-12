94cb426ecaef026d9397fa549c78851c756417c5
Sen-Zhang <solowolf21@126.com>
2016-09-12 14:51:27 -0700

    config rack-cors

--- back_end/portfolio_rails_api/config/application.rb
+++ back_end/portfolio_rails_api/config/application.rb
@@ -12,5 +12,12 @@ module PortfolioRailsApi
   class Application < Rails::Application
     config.api_only = true
     config.autoload_paths << Rails.root.join('lib')
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
+      end
+    end
   end
 end
